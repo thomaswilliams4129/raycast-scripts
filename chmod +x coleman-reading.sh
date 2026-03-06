bdf61d48d5511532a8c5fa012ce6f595811e811d
Thomas Williams <thwilliams4129@gmail.com>
2026-03-06 08:07:50 -0500

    Script for reminder of coleman reading report is 6 digits

---
+++ chmod +x coleman-reading.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title coleman reading
+# @raycast.mode inline
+#
+# Optional parameters:
+# @raycast.icon 📋
+# @raycast.packageName Raycast Scripts
+
+echo "6 digits"
